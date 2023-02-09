979ef41172fb00c711703dacfb92ec5ed33781f3
abaduser <Isaac.K.Masood@gmail.com>
2023-02-08 17:53:18 -0800

    uhh.. working out features. lazy git. better

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::env;
+use std::env::{self, vars_os};
 
 use serenity::async_trait;
 use serenity::client::{Client, Context};
+use serenity::framework::standard::help_commands::Command;
 use serenity::prelude::*;
 use serenity::model::{
+    guild::Member,
+    guild::Role,
     channel::Message,
     gateway::Ready};
 use serenity::framework::standard::{
@@ -12,26 +15,27 @@ use serenity::framework::standard::{
     macros::{command, group}
 };
 
-const NONSENSE: [&str;6] = ["quake3 net", "Q3 net", "unlag", "osp net", "cpma unlag", "cpma netcode"];
-
-
-#[command]
-async fn welcome(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Welcome you filthy animal").await?;
-    Ok(())
-}
-#[group]
-#[commands(welcome)]
-struct General;
+const NONSENSE: [&str;8] = ["quake3 net", "quake 3 net", "q 3 net", "q3 net", "unlag", "osp net", "cpma unlag", "cpma netcode"];
+const Q3_REPLY_STRING: &str = "Quake3 is shit\nhttps://cdn.discordapp.com/attachments/1039074867816955914/1067954505737568306/halfspeedserverview1.mp4\nPlay QuakeLive for the most up to date netcode";
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
+
+    async fn guild_member_addition(&self, ctx:Context, member:Member){
+        let info = format!("Hello {}, welcome to The Dojo. We approve new members manually, so while you wait check out https://thedojo.ninja/ for information about us and our servers.", member.display_name());
+        match member.user.direct_message(ctx, |m| m.content(info)).await{
+            Err(e) => println!("Err sending dm!\n{:?}",e),
+            _ => ()
+        }
+    }
+
     async fn message(&self, ctx:Context, msg: Message){
+        if env::var_os("FUCKQ3").unwrap().into_string() == "YES"
         for n in NONSENSE{
             if msg.content.to_lowercase().contains(n) && !msg.author.bot {
-                msg.reply_ping(&ctx, "Sorry but I was programmed to explain why Quake3 netcode is shite\nhttps://cdn.discordapp.com/attachments/1039074867816955914/1067954505737568306/halfspeedserverview1.mp4")
+                msg.reply_ping(&ctx, Q3_REPLY_STRING)
                 .await
                 .expect("Err processing nonsense");
                 return;
@@ -43,6 +47,9 @@ impl EventHandler for Handler {
         println!("{} has arrived.", ready.user.name);
     }
 }
+#[group]
+#[commands(lfd)]
+struct General;
 
 #[tokio::main]
 async fn main() {
@@ -50,11 +57,11 @@ async fn main() {
     let framework: StandardFramework = StandardFramework::new()
         .configure(|c| c.prefix("!"))
         .group(&GENERAL_GROUP);
-    let bot_token = match  env::var_os("DOJO_BOT_TOKEN").unwrap().into_string() {
-        Ok(s) => s,
-        Err(e) => panic!("{:?}", e)
-    };
-    //assert!(serenity::utils::validate_token(&bot_token).is_ok());
+    let bot_token = env::var_os("DOJO_BOT_TOKEN")
+        .expect("No Token set")
+        .into_string()
+        .unwrap();
+
     let intents = GatewayIntents::non_privileged() 
         | GatewayIntents::MESSAGE_CONTENT
         | GatewayIntents::GUILD_MESSAGES ;
@@ -69,4 +76,20 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("Err {:?}", why);
     }    
+}
+
+
+#[command]
+#[description = "Pings people around your rating for duel"]
+#[aliases("lookingforduel", "duel")]
+async fn lfd(ctx: &Context, msg : &Message) -> CommandResult{
+    let m = msg.member(ctx).await?;
+    let member_roles: Vec<Role> = m.roles(ctx).unwrap();
+    let mut highest_position = 0i64;
+    for role in member_roles{
+        if role.position > highest_position { highest_position = role.position; }
+    }
+
+
+    return Ok(())
 }
